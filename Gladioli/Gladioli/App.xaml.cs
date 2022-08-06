b228598bc15ab0960c3364396f12c84d9ea927a9
Sinobom-Plaatjie <4270448@myuwc.ac.za>
2022-08-06 19:23:18 +0200

    Added all the projects pages needed

--- Gladioli/Gladioli/App.xaml.cs
+++ Gladioli/Gladioli/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Gladioli.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace Gladioli
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MyJourneyPage());
         }
 
         protected override void OnStart()
